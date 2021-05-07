e0c9b7069fbe85e703ffac7941c4fd43faefff76
Everton <evertongmoraes@gmail.com>
2021-05-07 11:27:46 -0300

    Update MainActivity.java

--- app/src/main/java/com/descontron/lottotron/MainActivity.java
+++ app/src/main/java/com/descontron/lottotron/MainActivity.java
@@ -15,13 +15,13 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView[] textExtract;
+    private TextView textExtract;
     private TextView textExtract1;
     private TextView textExtract2;
     private TextView textExtract3;
     private TextView textExtract4;
 
-    private ImageView[] ballExtract;
+    private ImageView ballExtract;
     private ImageView ballExtract1;
     private ImageView ballExtract2;
     private ImageView ballExtract3;
@@ -44,26 +44,32 @@ public class MainActivity extends AppCompatActivity {
     private Button btnDraw;
 
     int numOfNumbers = 5;
-//---------------------------
-    private Random rnd = new Random();
-    private List<Integer> numberDraw = new ArrayList<>();
-    int count = 0;
-//---------------------------------
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for(int i=0; i<numOfNumbers;i++){
-            textExtract[i] = findViewById(textOutput[i]);
-            ballExtract[i] = findViewById(ballOutput[i]);
-        }
-//----------------------------------------------------------------
-        for(int i=0; i< numOfNumbers; i++) {
-            Log.i("RND", "Randon value" + i + (rnd.nextInt(50) + 1));
-        }
+        btnDraw = findViewById(R.id.button_draw);
+
+        textExtract = findViewById(R.id.text_extract);
+        textExtract1 = findViewById(R.id.text_extract1);
+        textExtract2 = findViewById(R.id.text_extract2);
+        textExtract3 = findViewById(R.id.text_extract3);
+        textExtract4 = findViewById(R.id.text_extract4);
+
+        ballExtract = findViewById(R.id.ball_extract);
+        ballExtract1 = findViewById(R.id.ball_extract1);
+        ballExtract2 = findViewById(R.id.ball_extract2);
+        ballExtract3 = findViewById(R.id.ball_extract3);
+        ballExtract4 = findViewById(R.id.ball_extract4);
+
+
+
+
         btnDraw.setOnClickListener(new View.OnClickListener() {
+
             private Random rnd = new Random();
             private List<Integer> numberDraw = new ArrayList<>();
             int count = 0;
@@ -73,7 +79,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 while(count< numOfNumbers){
-                    int rndNumber = rnd.nextInt(50)+1;
+
+                    for(int i=0; i< numOfNumbers; i++) {
+                        Log.i("RND", "Randon value" + i + " -> " + + (rnd.nextInt(50) + 1));
+                        count++;
+                    }
 
                 }
 
