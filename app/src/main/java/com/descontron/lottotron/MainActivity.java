98801f539ad1b32675f8f084fa349e5323f68c4a
Everton <evertongmoraes@gmail.com>
2021-05-10 10:08:32 -0300

    sorteio criado
    Sorteando e conferindo os resultados

--- app/src/main/java/com/descontron/lottotron/MainActivity.java
+++ app/src/main/java/com/descontron/lottotron/MainActivity.java
@@ -3,12 +3,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView[] textExtract;
+    private ImageView[] ballExtract;
+    private Button btnSorteio;
+    private TextView textPercent;
+
+    int numOfNumbers = 5;
 
     public void validaCorreto(int n, Context ct) {
         if ((n == 0) || (n > 50)) {
@@ -20,9 +35,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EditText Edt1, Edt2, Edt3, Edt4, Edt5;
-        Button btnSorteio;
+
         setContentView(R.layout.activity_main);
         Context contexto = this;
+        textExtract = new TextView[]{
+                findViewById(R.id.text_extract),
+                findViewById(R.id.text_extract1),
+                findViewById(R.id.text_extract2),
+                findViewById(R.id.text_extract3),
+                findViewById(R.id.text_extract4)};
+
+        textPercent = findViewById(R.id.text_percent);
+
+        ballExtract = new ImageView[]{
+                findViewById(R.id.ball_extract),
+                findViewById(R.id.ball_extract1),
+                findViewById(R.id.ball_extract2),
+                findViewById(R.id.ball_extract3),
+                findViewById(R.id.ball_extract4)};
+
         Edt1 = (EditText) findViewById(R.id.text_input_num1); Edt2 = (EditText) findViewById(R.id.text_input_num2);
         Edt3 = (EditText) findViewById(R.id.text_input_num3); Edt4 = (EditText) findViewById(R.id.text_input_num4);
         Edt5 = (EditText) findViewById(R.id.text_input_num5); btnSorteio = (Button) findViewById(R.id.button_draw);
@@ -72,20 +103,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         btnSorteio.setOnClickListener(new View.OnClickListener() {
+            private Random rnd = new Random();
+            int numberDraw;
+            private List<Integer> numbersDraw = new ArrayList<>();
+            int count = 0;
+            int acertos = 0;
+
+
+
             @Override
             public void onClick(View v) {
                 int n1,n2,n3,n4,n5;
+                List<Integer> nums = new ArrayList<>();
                 if ((! Edt1.getText().toString().equals("")) && (! Edt2.getText().toString().equals("")) && (! Edt3.getText().toString().equals("")) && (! Edt4.getText().toString().equals("")) && (! Edt5.getText().toString().equals(""))) {
-                    n1 = Integer.parseInt(Edt1.getText().toString());
-                    n2 = Integer.parseInt(Edt2.getText().toString());
-                    n3 = Integer.parseInt(Edt3.getText().toString());
-                    n4 = Integer.parseInt(Edt4.getText().toString());
-                    n5 = Integer.parseInt(Edt5.getText().toString());
-                    if ((n1 <= 50) && (n2 <= 50) && (n3 <= 50) && (n4 <= 50) && (n5 <= 50)) {
-                        if ((n1 != n2) && (n1 != n3) && (n1 != n4) && (n1 != n5)) {
-                            if ((n2 != n3) && (n2 != n4) && (n2 != n5)) {
-                                if ((n3 != n4) && (n3 != n5)) {
-                                    if ((n4 != n5)) {
+                    nums.add(Integer.parseInt(Edt1.getText().toString()));
+                    nums.add(Integer.parseInt(Edt2.getText().toString()));
+                    nums.add(Integer.parseInt(Edt3.getText().toString()));
+                    nums.add(Integer.parseInt(Edt4.getText().toString()));
+                    nums.add(Integer.parseInt(Edt5.getText().toString()));
+                    if ((nums.get(0) <= 50) && (nums.get(1) <= 50) && (nums.get(2) <= 50) && (nums.get(3) <= 50) && (nums.get(4) <= 50)) {
+                        if ((nums.get(0) != nums.get(1)) && (nums.get(0) != nums.get(2)) && (nums.get(0) != nums.get(3)) && (nums.get(0) != nums.get(4))) {
+                            if ((nums.get(1) != nums.get(2)) && (nums.get(1) != nums.get(3)) && (nums.get(1) != nums.get(4))) {
+                                if ((nums.get(2) != nums.get(3)) && (nums.get(2) != nums.get(4))) {
+                                    if ((nums.get(3) != nums.get(4))) {
 
                                         // POnto onde todas as validações estao ok e o sorteio pode ser feito
                                         // Numeros escolhidos sao: n1, n2, n3, n4, n5
@@ -93,6 +133,36 @@ public class MainActivity extends AppCompatActivity {
                                         Toast toast = Toast.makeText(contexto, "Numeros diferentes, ok !", 30);
                                         Edt5.setTextColor(Color.BLACK);
                                         toast.show();
+
+                                        while(count< numOfNumbers){
+                                            //numberDraw = rnd.nextInt(50)+1;
+                                            numberDraw = count+1;
+
+                                            if(!numbersDraw.contains(numberDraw)){
+                                                numbersDraw.add(numberDraw);
+                                                count++;
+                                            }
+
+                                        }
+
+                                        Collections.sort(numbersDraw);
+
+                                        for(int i=0; i < numOfNumbers; i++) {
+                                            textExtract[i].setText(numbersDraw.get(i).toString());
+                                            textExtract[i].setVisibility(View.VISIBLE);
+                                            ballExtract[i].setVisibility(View.VISIBLE);
+
+                                        }
+                                        for(int i=0; i < numOfNumbers;i++){
+                                            if(nums.contains(numbersDraw.get(i))){
+                                                textExtract[i].setTextColor(Color.parseColor("#FF0000"));
+                                                acertos++;
+                                            }
+                                        }
+
+                                        textPercent.setText(acertos + " acertos");
+                                        textPercent.setVisibility(View.VISIBLE);
+
                                     }
                                 }
                             }
@@ -109,7 +179,9 @@ public class MainActivity extends AppCompatActivity {
                         Toast toast = Toast.makeText(contexto, "Por favor, preencha todos os campos", 30);
                         toast.show();
                 }
-        }
+
+
+            }
         });
     }
 }
